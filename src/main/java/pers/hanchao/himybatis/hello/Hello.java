6c38e848ca7d67b81060e4b3501fcb1990ce3aad
hanchao5272 <89298141@qq.com>
2018-01-26 19:10:33 +0800

    添加MyBatis的入门实例
    1.通过mybatis-config.xml去总控MyBatis配置：配置持久化对象及别名、数据源信息以及持久化对象的映射文件
    2.通过Question.xml去配置持久化对象Question的SQL操作及SQL语句
    3.通过Resources.getResourceXXXXX读取MyBatis总配置文件
    4.通过SqlSession.XXXX去执行数据库操作

---
+++ src/main/java/pers/hanchao/himybatis/hello/Hello.java
@@ -0,0 +1,58 @@
+package pers.hanchao.himybatis.hello;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.Logger;
+
+import java.io.Reader;
+
+/**
+ * <p>简单的MyBaits实例：不依赖Spring和Spring MVC</p>
+ * @author hanchao 2018/1/26 19:02
+ **/
+public class Hello {
+
+    /** MyBatis通过SqlSessionFactory创建SqlSession进而进行数据库操作 */
+    private static SqlSessionFactory sqlSessionFactory;
+    /** 读取并加载MyBatis的配置文件 */
+    private static Reader reader;
+    private static final Logger LOGGER = Logger.getLogger(Hello.class);
+
+    static {
+        try{
+            //读取MyBatis的配置文件，总配置文件通过mappers属性依次读取分配置文件
+            reader = Resources.getResourceAsReader("mybatis-config.xml");
+            //创建SqlSessionFactory
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        }catch (Exception e){
+            e.printStackTrace();
+            LOGGER.error("找不到MyBatis的配置文件！");
+        }
+    }
+
+    public static SqlSessionFactory getSqlSessionFactory() {
+        return sqlSessionFactory;
+    }
+
+    /**
+     * <p>读取id=99999的试题信息</p>
+     * @author hanchao 2018/1/26 19:09
+     **/
+    public static void main(String[] args) {
+        //创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try{
+            //执行查询方法
+            Question question = sqlSession.selectOne("pers.hanchao.himybatis.hello.QuestionMapper.GetQuestionById",99999);
+            if (null != question){
+                //输出结果
+                LOGGER.info(question.toString());
+            }
+        }finally {
+            //关闭SqlSession
+            sqlSession.close();
+        }
+    }
+}
